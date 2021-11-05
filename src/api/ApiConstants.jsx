33b108ed13284349738b6229b907af38b33bb3a7
barnaholl <hollbarna@freemail.hu>
2021-11-05 08:47:18 +0100

    console.log tokens

--- src/api/ApiConstants.jsx
+++ src/api/ApiConstants.jsx
@@ -1,5 +1,5 @@
-const BASE_URL="https://typ-project-art.herokuapp.com/";
-//const BASE_URL="http://localhost:8080/";
+//const BASE_URL="https://typ-project-art.herokuapp.com/";
+const BASE_URL="http://localhost:8080/";
 
 export{
     BASE_URL
