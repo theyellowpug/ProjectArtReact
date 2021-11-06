8838d577e93f2caf9a57602525a069c38444b22a
barnaholl <hollbarna@freemail.hu>
2021-11-06 11:21:22 +0100

    refresh accessTokens

--- src/api/AuthenticationApi.jsx
+++ src/api/AuthenticationApi.jsx
@@ -1,10 +1,20 @@
 import axios from "axios";
 import {BASE_URL} from "./ApiConstants"
 
+
+const config = {
+    headers: { Authorization: `Bearer ${localStorage.getItem("refresh_token")}` }
+};
+
 const login = async (userCredentials) => {
-    return await axios.post(BASE_URL + "login", null,{ params: userCredentials});
+    return await axios.post(BASE_URL + "login", null, { params: userCredentials });
+}
+
+const refreshToken = async () => {
+    return await axios.get(BASE_URL + "client/refreshToken", config);
 }
 
 export{
     login,
+    refreshToken
 }
--- src/component/authentication/LoginForm.jsx
+++ src/component/authentication/LoginForm.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux"
+import { bindActionCreators } from 'redux';
 import { login } from "../../api/AuthenticationApi";
-import { getJwtToken, JwtManager, setJwtToken, token } from "./JwtManager";
+import { JwtTokenActionCreators } from "../../state/actions/JwtTokenActions";
 
 export const LoginForm = (props) => {
+
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const { setJwtToken, removeJwtToken , refreshJwtToken} = bindActionCreators( JwtTokenActionCreators, dispatch);
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -15,6 +21,7 @@ export const LoginForm = (props) => {
       }
       login(userCredentials).then(response=>{
         setJwtToken(response.data.access_token)
+        localStorage.setItem("refresh_token", response.data.refresh_token)
       })
   }
 
@@ -25,12 +32,7 @@ export const LoginForm = (props) => {
           <input onChange={event=>setUsername(event.target.value)}></input>
           <label>Password:</label>
           <input onChange={event=>setPassword(event.target.value)} type="password"></input>
-
           <button onClick={submitLoginForm}>Login</button>
       </form>
-      <button onClick={getJwtToken}>getToken</button>
-
     </div>)
-
-
 }
