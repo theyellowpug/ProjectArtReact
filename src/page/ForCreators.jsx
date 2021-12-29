1c68d197fa6e651bb566fb3521d603aea041ad09
barnaholl <hollbarna@freemail.hu>
2021-12-29 14:04:41 +0100

    redirect to login or profile

--- src/page/ForCreators.jsx
+++ src/page/ForCreators.jsx
@@ -2,26 +2,20 @@ import React, {useState,useEffect} from 'react';
 import { useSelector } from "react-redux";
 import jwt_decode from "jwt-decode";
 import { getClientIdByEmail } from '../api/ClientApi';
+import { useHistory } from 'react-router-dom';
 
 
 export default function ForCreators() {
 
+    const history = useHistory()
+
     const state = useSelector((state) => state);
-    const [clientId,setClientId] = useState("");
 
-    const decodeJWtToken = () => {
+    useEffect(() => {
         state.accessToken!=="" ? 
-            getClientIdByEmail(jwt_decode(state.accessToken).sub).then(respone=>setClientId(respone.data)) : setClientId("")
-    }
-
-    useEffect(()=>{
-        decodeJWtToken()
+            getClientIdByEmail(jwt_decode(state.accessToken).sub).then(respone=>history.push("/profile/"+respone.data)) : history.push("/login")
     },[state])
 
-    return(
-        clientId!=="" ? 
-            <p>Logged in as: {clientId}</p> //redirect to profile/id page
-        :
-            <p>Not logged in</p>
-    )
+    return(<></>)
+
 }
