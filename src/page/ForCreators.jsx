c2b62017f411673d6a6cd16e1b11ec96ff0cc52a
barnaholl <hollbarna@freemail.hu>
2021-12-29 12:17:20 +0100

    decode jwt

--- src/page/ForCreators.jsx
+++ src/page/ForCreators.jsx
@@ -1,19 +1,23 @@
 import React, {useState,useEffect} from 'react';
 import { useSelector } from "react-redux";
+import jwt_decode from "jwt-decode";
 
 
 export default function ForCreators() {
 
     const state = useSelector((state) => state);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [clientEmail,setClientEmail] = useState();
 
     useEffect(()=>{
         setIsLoggedIn(state.accessToken!=="")
+        let decode = jwt_decode(state.accessToken)
+        setClientEmail(decode.sub)
     },[state])
 
     return(
         isLoggedIn ? 
-            <p>Logged in</p> //redirect to profile/id page
+            <p>Logged in as: {clientEmail}</p> //redirect to profile/id page
         :
             <p>Not logged in</p>
     )
